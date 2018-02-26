4a19615ace728ecdcfc8568f2fc833f797560e40
Adrian Fernandez Hernandez <adrian@X550CL.mipractica.es>
2018-02-26 14:07:48 +0100

    Proyecto vacio

---
+++ ttl/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(){
+        // Activar integridad referencial
+        if (config('database.default' ) == 'sqlite') {
+            $db = app()->make('db');
+            $db->connection ()->getPdo()->exec("PRAGMA foreign_keys = ON" );
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
