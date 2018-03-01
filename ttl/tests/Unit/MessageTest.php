e446a7b2fb206571203eba9c39ef5d79dbe91bba
Kamil <futureamv@gmail.com>
2018-03-01 23:52:48 +0100

    kbmh1

---
+++ ttl/tests/Unit/MessageTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MessageTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDataExists()
+    {
+        $message = Message::where ('user_id', '0')->first();
+        $this->asserNull($message);
+
+        $message = Message::where('title','Test1')->first();
+        $this->assertEquals($message->text,'TextTest1');
+        $this->assertEquals($message->read,false);
+        $this->assertEquals($message->date, '2017-07-07 11:23:19.120');
+        $this->assertEquals($message->user_id, 'mike@gmail.com');
+
+        $message = Message::find(2);
+        $user = User::where('email','kamil@gmail.com')->first();
+        $this->assertEquals($message->user_id,$user->id);
+        $this->assertEquals($message->title,'Test2');
+        $this->assertEquals($message->text,'TextTest2');
+        $this->assertEquals($message->read,true);
+        $this->assertEquals($message->user_id, 'kamil@gmail.com');
+
+        $message = Message::find(3);
+        $user = User::where('email','cristian@gmail.com')->first();
+        $this->assertEquals($message->user_id,$user->id);
+        $this->assertEquals($message->title,'Test3');
+        $this->assertEquals($message->text,'TextTest3');
+        $this->assertEquals($message->read,true);
+        $this->assertEquals($message->user_id, 'cristian@gmail.com');
+        $this->assertEquals($message->date,'2409-07-07 11:23:19.120');
+        
+        $message = Message::find(4);
+        $this->assertNull($message);
+
+    }
+}
---
+++ ttl/tests/Unit/MessageUserTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MessageUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function getMessageReceivers($id)
+    {
+        $receivers = array();
+
+        if (Message::find($id) == null )
+        {
+            return null;
+        }
+
+        $receivers = Message::find($id)->user_receive()->get();
+
+
+        foreach ($receivers as $r)
+        {
+            array_push($receivers, $r);
+        }
+
+        return $receivers;
+    }
+
+    public function getMessageSent($userId)
+    {
+        $sent = array();
+
+        if (User::find($userId) == null)
+        {
+            return null;
+        }
+
+        $sent = User::find($userId)->messageSent()->get();
+
+
+        foreach ($sent as $s)
+        {
+            array_push($sent, $s);
+        }
+
+        return $sent;
+    }
+
+
+    public function testMessageReceivers()
+    {
+        $user = User::where('email','mike@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $receivers = $this->getMessageReceivers($message->id);
+        $this->assertNotNull($receivers);
+        $this->assertEquals($receivers[0]->email,'kamil@gmail.com');
+        $this->assertNull($receivers[1]);
+
+        $user = User::where('email','kamil@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $receivers = $this->getMessageReceivers($message->id);
+        $this->assertNotNull($receivers);
+        $this->assertEquals($receivers[0]->email,'mike@gmail.com');
+        $this->assertEquals($receivers[1]->email,'tudor@gmail.com');
+        $this->assertNull($receivers[2]);
+
+        $user = User::where('email','cristian@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $receivers = $this->getMessageReceivers($message->id);
+        $this->assertNotNull($receivers);
+        $this->assertEquals($receivers[0]->email,'adrian@gmail.com');
+        $this->assertNull($receivers[1]);
+
+
+
+    }
+
+    public function testMessageSender()
+    {
+        
+        $user = User::where('email','mike@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $sent = $this->getMessageSent($user->id);
+        $this->assertNotNull($sent);
+        $this->assertEquals($message,$sent[0]);
+        $this->assertNull($sent[1]);
+
+        $user = User::where('email','kamil@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $sent = $this->getMessageSent($user->id);
+        $this->assertNotNull($sent);
+        $this->assertEquals($message,$sent[0]);
+        $this->assertNull($sent[1]);
+
+        $user = User::where('email','cristian@gmail.com')->first();
+        $message = Message::where('user_id',$user->id)->first();
+        $sent = $this->getMessageSent($user->id);
+        $this->assertNotNull($sent);
+        $this->assertEquals($message,$sent[0]);
+        $this->assertEquals('Test3',$sent[0]->title);
+        $this->assertEquals('Test4',$sent[1]->title);
+        $this->assertNull($sent[2]);
+
+    }
+}
