08ed328e8bd92dbd3eb9d62b5a7d9f3e0a2b08c3
tudor_mateiu <tnm5@alu.ua.es>
2018-03-16 13:38:23 +0100

    Migracion y Modelo de Song

---
+++ ttl/app/Song.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    public function user_uploaded(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function status(){
+        return $this->hasMany('App\User');
+    }
+}
