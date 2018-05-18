8bfbee02408da4103abb7b132d9752c35cc43415
Kamil <futureamv@gmail.com>
2018-05-18 21:17:04 +0200

    Pagina de informacion

---
+++ ttl/resources/views/information.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="/favicon.png">
+    <title>Time to Listen</title>
+    <link rel="stylesheet" type="text/css" href="/css/root.css">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    @include('general-css')
+</head>
+<body>
+    <div id="img-div">
+        <img src="line.png">
+    </div>
+    <div id="content">
+        <div id="information">
+                <h1 style="text-align: center;">We Are Glad To Hear You!</h3>
+                <h2 style="text-align: center;">Connect on Time to Listen</h4>
+            <div style="margin-top:5%; font-size: 20px; text-align: justify">
+                <p style="text-size: 20px">Time to Listen It is a project developed by students of 
+                    the University of Alicante (Mansouri Kamil,Fernández Hernández Adrián, Mateiu Tudor Nicolae and 
+                    García Romero Cristian)</p>
+
+            </div>
+            <div style="margin-top: 5%;text-align:center">
+                <p style=" position: relative;float: right; margin-right: 35%;"><a href="/loginsignup"> Sign up for free </a></p>
+                <p style=" position: relative;float: left;margin-left: 35%;"><a href="/"> Home </a></p>
+            </div>
+
+        </div>
+        
+        
+    </div>
+
+    <div id="general-footer-height"></div>
+    <div id="general-footer">
+        <div style=" position: relative;float: left; margin-left:35%">   
+            <p>Design of Software System</p>
+            <p>2017 - 2018</p>
+            <p>Time to Listen</p>
+        </div>
+        <div style=" position: relative;float: right;margin-right: 35%;">
+            <p style="margin-top: 20%"><a href="/contact"> Contact us </a></p>
+            <p><a href="/information"> More information </a></p>
+        </div>
+    </div>
+</body>
+</html>
